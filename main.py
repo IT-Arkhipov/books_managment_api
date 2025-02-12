c8bcc9f2a73e1a972b02fb8d3144a9a7019f8256
Andrei Arkhipov <iamckuff@gmail.com>
2025-02-12 22:09:22 +0300

    add: запуск сервера

---
+++ main.py
@@ -0,0 +1,12 @@
+import uvicorn
+from fastapi import FastAPI, APIRouter
+from src.routes import router
+
+
+app = FastAPI()
+api_v1_router = APIRouter(prefix='/api/v1')
+api_v1_router.include_router(router)
+app.include_router(api_v1_router)
+
+if __name__ == '__main__':
+    uvicorn.run('main:app', host='127.0.0.1', port=8000, log_level='info', reload=True)
