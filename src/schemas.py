89f9ee03ecf82b90f9606e6332faa23f7048b605
Andrei Arkhipov <iamckuff@gmail.com>
2025-02-20 22:07:19 +0300

    add: Books schema edit: book id to book_id: str

--- src/schemas.py
+++ src/schemas.py
@@ -2,7 +2,11 @@ from pydantic import BaseModel
 
 
 class Book(BaseModel):
-    id: int
+    book_id: str
     title: str
     author: str
     year: int
+
+
+class Books(BaseModel):
+    books: list[Book]
