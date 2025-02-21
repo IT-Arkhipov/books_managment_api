7ed639873b7b3d0c213927799ccd5c2c455a2118
Andrei Arkhipov <iamckuff@gmail.com>
2025-02-21 13:40:13 +0300

    edit: result by hidden params 'offset:limit' and 'search' string in title, author, publisher

--- src/utils.py
+++ src/utils.py
@@ -5,9 +5,21 @@ from src.books_db import books
 
 
 def get_books(
-        offset: int = Query(default=0, ge=0),
-        limit: int = Query(default=5, ge=1, le=10)) -> schemas.Books:
-    paginated_books = books[offset:offset+limit]
+        offset: int = Query(default=0, ge=0, include_in_schema=False),
+        limit: int = Query(default=0, ge=0, include_in_schema=False),
+        search: str = Query(default="", include_in_schema=False),
+) -> schemas.Books:
+    if (offset + limit) == 0:
+        top_index = len(books)
+    else:
+        top_index = offset + limit
+    paginated_books = books[offset:top_index]
+    selected_books = [
+        book for book in paginated_books if
+        search.lower() in book["title"].lower() or
+        search.lower() in book["author"].lower() or
+        search.lower() in book["publisher"].lower()
+    ]
     return schemas.Books(
-        books=[schemas.Book(**book) for book in paginated_books]
+        books=[schemas.Book(**book) for book in selected_books]
     )
