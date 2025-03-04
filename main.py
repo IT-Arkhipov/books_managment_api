7770df4bf1806bb8093b7e4b7faa1f557d9d0948
Andrei Arkhipov <iamckuff@gmail.com>
2025-03-04 10:00:48 +0300

    add: CORSMiddleware

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 import uvicorn
 from fastapi import FastAPI, APIRouter
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.api.books.routes import router as books_router
 from src.api.users.routes import router as users_router
 
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET", "POST", "PUT", "DELETE"],
+    allow_headers=["*"],
+)
+
+
 api_v1_router = APIRouter(prefix='/api/v1')
 api_v1_router.include_router(users_router)
 api_v1_router.include_router(books_router)
