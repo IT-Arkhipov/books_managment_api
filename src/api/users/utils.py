4b17b6d41d30705f9b8a1304ffad3bc27402cda9
Andrei Arkhipov <iamckuff@gmail.com>
2025-03-04 10:01:23 +0300

    edit: create or update user by it's email

--- src/api/users/utils.py
+++ src/api/users/utils.py
@@ -1,6 +1,9 @@
 from typing import List
 
+from fastapi import Depends
+
 from src.api.users import schemas
+from src.api.users.auth import is_admin
 from src.data.users_db import users
 
 
@@ -8,6 +11,9 @@ def get_users() -> List[schemas.User]:
     return [schemas.GetUser(**user) for user in users]
 
 
-def create_user(user: schemas.User):
-    users.append(user.model_dump())
-    return user
+def create_user(new_user: schemas.User, is_admin_role=Depends(is_admin)):
+    for user in users:
+        if user.get('email') == new_user.email:
+            users.remove(user)
+    users.append(new_user.model_dump())
+    return new_user
